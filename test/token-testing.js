5ba751a699625f03fec019aff05d742c29859fe5
Felipe Benghi <fbenghi@gmail.com>
2021-06-21 22:37:52 -0300

    Test: Checking contract owner

--- test/token-testing.js
+++ test/token-testing.js
@@ -1,5 +1,24 @@
 const { expect } = require("chai");
 
+describe("Deploy", function() {
+  it("Contract owner must be the one of the deployer", async function() {
+
+    const accounts = await ethers.getSigners();
+
+    const account = accounts[0];
+    console.log(account.address);
+
+    const Token = await ethers.getContractFactory("Token", account);
+    const token = await Token.deploy();
+    
+    await token.deployed();
+
+    
+    expect(await token.owner()).to.equal(account.address);
+  });
+});
+
+
 describe("Token", function() {
   it("Should return the total token supply", async function() {
 
